6130870e4e3da89f86b3bc4edc401e7d8d75f4e0
Rodrigo Nicola <rodrigo.nicola@softrh.com.br>
2022-04-22 16:29:47 -0300

    test new version v1.8

--- check_pypi_versions.py
+++ check_pypi_versions.py
@@ -3,6 +3,7 @@ import re
 import requests as req
 from pkg_resources import parse_version
 
+import os
 import json
 
 
@@ -19,31 +20,38 @@ def get_versions(package_name, last_release=None):
     return versions
 
 
+path_to_requirements = 'requirements.txt'
+
 pkgs_to_bump = []
 pkgs_ok = []
 
-with open('requirements.txt', 'r') as requirements:
-    for pkg in requirements.readlines():
-        pkg.replace('\n', '')
+if os.path.isfile(path_to_requirements):
 
-        if pkg.find('==') != -1:
-            pkg, version = pkg.split('==')
-        else:
-            pattern_1 = re.findall(r"(\d+(?:\.\d+){0,2})", pkg)
-            if (pattern_1):
-                version = pattern_1
-                pkg = re.findall(r'^(.*?)>=', pkg)[0]
-            else:
-                continue
+    with open('requirements.txt', 'r') as requirements:
+        file_lines = (
+            requirements.readlines() if requirements else [])
 
-        if isinstance(version, (str, bytes)):
-            latest_release = get_versions(pkg, True)
-            if parse_version(version) < latest_release:
-                pkgs_to_bump.append({
-                    "package": pkg,
-                    "bump_to": str(latest_release)
-                })
+        for pkg in file_lines:
+            pkg.replace('\n', '')
+
+            if pkg.find('==') != -1:
+                pkg, version = pkg.split('==')
             else:
-                pkgs_ok.append(pkg)
+                pattern_1 = re.findall(r"(\d+(?:\.\d+){0,2})", pkg)
+                if (pattern_1):
+                    version = pattern_1
+                    pkg = re.findall(r'^(.*?)>=', pkg)[0]
+                else:
+                    continue
+
+            if isinstance(version, (str, bytes)):
+                latest_release = get_versions(pkg, True)
+                if parse_version(version) < latest_release:
+                    pkgs_to_bump.append({
+                        "package": pkg,
+                        "bump_to": str(latest_release)
+                    })
+                else:
+                    pkgs_ok.append(pkg)
 
 print(json.dumps(pkgs_to_bump))
